f7411a892423f80a0b02d12a3dd2110f4f5dc7fd
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-12-13 11:28:20 -1000

    testing out to see if we can install gems from a script.

--- bin/setup-jenkins.rb
+++ bin/setup-jenkins.rb
@@ -1,9 +1,10 @@
 gems = %w{trollop watir cucumber}
 gems.each do |g|
   begin
-    gem g
-  rescue LoadError
     system("gem install #{g}")
     Gem.clear_paths
+    puts "Installed #{g}"
+  rescue LoadError
+    puts "Couldn't install #{g}"
   end
 end
