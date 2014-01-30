232b628099c9930387aa0b943702b4699eaeaffa
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-01-30 12:00:32 -0600

    changes to script to get the opsworks work stack working with the cfn template. before it was running the template but then not using the values. need to muck around with instance profiles to get everything working.

--- bin/create_jenkins_server_stack.rb
+++ bin/create_jenkins_server_stack.rb
@@ -53,7 +53,6 @@ while (stack_in_progress cfn_stack_name)
   print_and_flush "."
   sleep 10
 end
-
 puts
 
 # get the resource names out of the cfn stack so we can pass themto opsworks
@@ -65,104 +64,106 @@ end
 jenkins_security_group = resources["JenkinsSecurityGroupOutput"]
 ssh_security_group = resources["SSHSecurityGroupOutput"]
 servicerolearn = resources["ServiceRoleOutput"]
-ec2rolearn = resources["EC2RoleOutput"]
+ec2rolearn = resources["EC2RoleInstanceProfileOutput"]
 
 # create the opsworks stack
 ops = Aws::OpsWorks.new region: "us-east-1"
 
 # opsworks configuration is passed in as json
 custom_json = <<END
-{ 
-    "rvm": {
-        "user_installs": [
-            {
-                "user": "jenkins"
-            }
-        ],
-        "version": "1.22.19",
-        "user_home_root": "/var/lib",
-        "global_gems": [
-            {
-                "name": "trollop",
-                "version": "2.0"
-            },
-            {
-                "name": "aws-sdk-core",
-                "version": "2.0.0.rc2"
-            }
-        ]
-    },
-    "pipeline": {
-      "source" : "#{opts[:source]}"
-    },
+  { 
+      "rvm": {
+          "user_installs": [
+              {
+                  "user": "jenkins"
+              }
+          ],
+          "version": "1.22.19",
+          "user_home_root": "/var/lib",
+          "global_gems": [
+              {
+                  "name": "trollop",
+                  "version": "2.0"
+              },
+              {
+                  "name": "aws-sdk-core",
+                  "version": "2.0.0.rc2"
+              }
+          ]
+      },
+      "pipeline": {
+        "source" : "#{opts[:source]}"
+      },
 
-    "jenkins": {
-        "notifications": {
-            "enabled": "false"
-        },
-        "http_proxy": {
-          "variant" : "apache2"
-        },
-        "server": {
-          "plugins" : [
-                   { "name" : "analysis-core",                 "version" : "1.38"    },
-                   { "name" : "ansicolor",                     "version" : "0.3.1"   },
-                   { "name" : "audit-trail",                   "version" : "1.8"     },
-                   { "name" : "brakeman",                      "version" : "0.7"     },
-                   { "name" : "build-pipeline-plugin",         "version" : "1.4"     },
-                   { "name" : "buildresult-trigger",           "version" : "0.10"    },
-                   { "name" : "conditional-buildstep",         "version" : "1.3.1"   },
-                   { "name" : "config-file-provider",          "version" : "2.6.2"   },
-                   { "name" : "configurationslicing",          "version" : "1.38.3"  },
-                   { "name" : "cucumber-reports",              "version" : "0.0.21"  },
-                   { "name" : "email-ext",                     "version" : "2.35.1"  }, 
-                   { "name" : "envinject",                     "version" : "1.89"    },
-                   { "name" : "fstrigger",                     "version" : "0.34"    },
-                   { "name" : "git",                           "version" : "1.4.0"   },
-                   { "name" : "git-client",                    "version" : "1.0.6"   },
-                   { "name" : "github",                        "version" : "1.8"     },
-                   { "name" : "github-api",                    "version" : "1.44"    },
-                   { "name" : "groovy",                        "version" : "1.14"    },
-                   { "name" : "groovy-postbuild",              "version" : "1.8"     },
-                   { "name" : "htmlpublisher",                 "version" : "1.2"     },
-                   { "name" : "ivytrigger",                    "version" : "0.26"    },
-                   { "name" : "jenkins-cloudformation-plugin", "version" : "0.11"    },
-                   { "name" : "job-exporter" ,                 "version" : "0.4"     },
-                   { "name" : "jquery",                        "version" : "1.7.2-1" },
-                   { "name" : "log-parser",                    "version" : "1.0.8"   },
-                   { "name" : "managed-scripts" ,              "version" : "1.1"     }, 
-                   { "name" : "multiple-scms",                 "version" : "0.2"     },
-                   { "name" : "parameterized-trigger",         "version" : "2.20"    },
-                   { "name" : "postbuildscript",               "version" : "0.14"    },
-                   { "name" : "rake",                          "version" : "1.7.8"   },
-                   { "name" : "ruby-runtime",                  "version" : "0.12"    },
-                   { "name" : "rubyMetrics",                   "version" : "1.5.0"   },
-                   { "name" : "run-condition",                 "version" : "0.10"    },
-                   { "name" : "rvm",                           "version" : "0.4"     },
-                   { "name" : "scripttrigger",                 "version" : "0.28"    },
-                   { "name" : "token-macro",                   "version" : "1.6"     },
-                   { "name" : "urltrigger",                    "version" : "0.31"    },
-                   { "name" : "ws-cleanup",                    "version" : "0.18"    },
-                   { "name" : "xtrigger",                      "version" : "0.54"    }
-                  ],
+      "jenkins": {
+          "notifications": {
+              "enabled": "false"
+          },
+          "http_proxy": {
+            "variant" : "apache2"
+          },
+          "server": {
+            "plugins" : [
+                     { "name" : "analysis-core",                 "version" : "1.38"    },
+                     { "name" : "ansicolor",                     "version" : "0.3.1"   },
+                     { "name" : "audit-trail",                   "version" : "1.8"     },
+                     { "name" : "brakeman",                      "version" : "0.7"     },
+                     { "name" : "build-pipeline-plugin",         "version" : "1.4"     },
+                     { "name" : "buildresult-trigger",           "version" : "0.10"    },
+                     { "name" : "conditional-buildstep",         "version" : "1.3.1"   },
+                     { "name" : "config-file-provider",          "version" : "2.6.2"   },
+                     { "name" : "configurationslicing",          "version" : "1.38.3"  },
+                     { "name" : "cucumber-reports",              "version" : "0.0.21"  },
+                     { "name" : "email-ext",                     "version" : "2.35.1"  }, 
+                     { "name" : "envinject",                     "version" : "1.89"    },
+                     { "name" : "fstrigger",                     "version" : "0.34"    },
+                     { "name" : "git",                           "version" : "1.4.0"   },
+                     { "name" : "git-client",                    "version" : "1.0.6"   },
+                     { "name" : "github",                        "version" : "1.8"     },
+                     { "name" : "github-api",                    "version" : "1.44"    },
+                     { "name" : "groovy",                        "version" : "1.14"    },
+                     { "name" : "groovy-postbuild",              "version" : "1.8"     },
+                     { "name" : "htmlpublisher",                 "version" : "1.2"     },
+                     { "name" : "ivytrigger",                    "version" : "0.26"    },
+                     { "name" : "jenkins-cloudformation-plugin", "version" : "0.11"    },
+                     { "name" : "job-exporter" ,                 "version" : "0.4"     },
+                     { "name" : "jquery",                        "version" : "1.7.2-1" },
+                     { "name" : "log-parser",                    "version" : "1.0.8"   },
+                     { "name" : "managed-scripts" ,              "version" : "1.1"     }, 
+                     { "name" : "multiple-scms",                 "version" : "0.2"     },
+                     { "name" : "parameterized-trigger",         "version" : "2.20"    },
+                     { "name" : "postbuildscript",               "version" : "0.14"    },
+                     { "name" : "rake",                          "version" : "1.7.8"   },
+                     { "name" : "ruby-runtime",                  "version" : "0.12"    },
+                     { "name" : "rubyMetrics",                   "version" : "1.5.0"   },
+                     { "name" : "run-condition",                 "version" : "0.10"    },
+                     { "name" : "rvm",                           "version" : "0.4"     },
+                     { "name" : "scripttrigger",                 "version" : "0.28"    },
+                     { "name" : "token-macro",                   "version" : "1.6"     },
+                     { "name" : "urltrigger",                    "version" : "0.31"    },
+                     { "name" : "ws-cleanup",                    "version" : "0.18"    },
+                     { "name" : "xtrigger",                      "version" : "0.54"    }
+                    ],
 
-            "install_method": "package",
-            "version": "1.543-1.1"
-        },
-        "node": {
-          "executors": 8
-        }
-    }
-}
+              "install_method": "package",
+              "version": "1.543-1.1"
+          },
+          "node": {
+            "executors": 8
+          }
+      }
+  }
 END
 
 # create a new opsworks stack
 stack_params = {
   name: "Jenkins Server #{@timestamp}", 
   region: aws_region, 
+  # vpc_id: "vpc-e5455b87",
+  # default_subnet_id: "subnet-7087bc04",
   default_os: 'Amazon Linux',
-  service_role_arn: 'arn:aws:iam::324320755747:role/aws-opsworks-service-role', 
-  default_instance_profile_arn: 'arn:aws:iam::324320755747:instance-profile/jenkins',
+  service_role_arn: servicerolearn,
+  default_instance_profile_arn: ec2rolearn,
   custom_json: custom_json,
   use_custom_cookbooks: true,
   custom_cookbooks_source: {
@@ -183,7 +184,8 @@ layer_params = {
   type: 'custom',
   name: 'Jenkins Server Layer',
   shortname: 'jenkins',
-  custom_security_group_ids: [ jenkins_security_group, ssh_security_group ],
+  # custom_security_group_ids: [ jenkins_security_group, ssh_security_group ],
+  custom_security_group_ids: [  ],
   packages: %w{readline-devel libyaml-devel libffi-devel mlocate},
   custom_recipes: { setup: %w{ jenkins::server jenkins::proxy rvm::user_install jenkins-configuration::jobs jenkins-configuration::views opsworks_nodejs } }
 }
